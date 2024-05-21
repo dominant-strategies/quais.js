4b3e380a0ea7dd50daf380796c37968978b5ab16
Juuddi <justinrudi26@gmail.com>
2024-05-21 13:38:00 -0500

    Add prettier, eslint, husky, lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-    ],
-  };
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    parserOptions: {
+        project: './tsconfig.base.json',
+    },
+};
