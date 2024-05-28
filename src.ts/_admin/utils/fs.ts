432be6ba7aae950709a6b41def7aac5c4e8914a3
rileystephens28 <riley.stephens28@gmail.com>
2024-05-28 18:45:41 -0500

    Remove `_admin` directory

--- src.ts/_admin/utils/fs.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-import { resolve } from "./path.js";
-
-export function atomicWrite(path: string, value: string | Uint8Array): void {
-    const tmp = resolve(".atomic-tmp");
-    fs.writeFileSync(tmp, value);
-    fs.renameSync(tmp, path);
-}
