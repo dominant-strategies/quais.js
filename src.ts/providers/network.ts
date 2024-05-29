df391eac98d48920bfb2de792490eeac8d47a9e3
rileystephens28 <riley.stephens28@gmail.com>
2024-05-28 19:25:25 -0500

    Remove network plugin logic

--- src.ts/providers/network.ts
+++ src.ts/providers/network.ts
@@ -4,13 +4,9 @@
  * @_subsection: api/providers:Networks [networks]
  */
 
-import { getBigInt, assert, assertArgument } from '../utils/index.js';
-
-import { FetchUrlFeeDataNetworkPlugin, GasCostPlugin } from './plugins-network.js';
+import { getBigInt, assertArgument } from '../utils/index.js';
 
 import type { BigNumberish } from '../utils/index.js';
-
-import type { NetworkPlugin } from './plugins-network.js';
 
 /**
  * A Networkish can be used to allude to a Network, by specifing:
@@ -32,23 +28,6 @@ export type Networkish =
           chainId?: number;
       };
 
-/* * * *
-// Networks which operation against an L2 can use this plugin to
-// specify how to access L1
-// for example.
-export class LayerOneConnectionPlugin extends NetworkPlugin {
-    readonly provider!: Provider;
-// @TODO: Rename to ChainAccess and allow for connecting to any chain
-    constructor(provider: Provider) {
-        super("org.quais.plugins.layer-one-connection");
-        defineProperties<LayerOneConnectionPlugin>(this, { provider });
-    }
-
-    clone(): LayerOneConnectionPlugin {
-        return new LayerOneConnectionPlugin(this.provider);
-    }
-}
-*/
 
 const Networks: Map<string | bigint, () => Network> = new Map();
 
@@ -61,15 +40,12 @@ export class Network {
     #name: string;
     #chainId: bigint;
 
-    #plugins: Map<string, NetworkPlugin>;
-
     /**
      * Creates a new **Network** for `name` and `chainId`.
      */
     constructor(name: string, chainId: BigNumberish) {
         this.#name = name;
         this.#chainId = getBigInt(chainId);
-        this.#plugins = new Map();
     }
 
     /**
@@ -150,90 +126,12 @@ export class Network {
     }
 
     /**
-     * Returns the list of plugins currently attached to this Network.
-     */
-    get plugins(): Array<NetworkPlugin> {
-        return Array.from(this.#plugins.values());
-    }
-
-    /**
-     * Attach a new `plugin` to this Network. The network name must be unique, excluding any fragment.
-     *
-     * @param {NetworkPlugin} plugin - The plugin to attach.
-     *
-     * @returns {this} This Network instance.
-     */
-    attachPlugin(plugin: NetworkPlugin): this {
-        if (this.#plugins.get(plugin.name)) {
-            throw new Error(`cannot replace existing plugin: ${plugin.name} `);
-        }
-        this.#plugins.set(plugin.name, plugin.clone());
-        return this;
-    }
-
-    /**
-     * Return the plugin, if any, matching `name` exactly. Plugins with fragments will not be returned unless `name`
-     * includes a fragment.
-     *
-     * @param {string} name - The name of the plugin to get.
-     *
-     * @returns {NetworkPlugin | null} The plugin, or null if not found.
-     */
-    getPlugin<T extends NetworkPlugin = NetworkPlugin>(name: string): null | T {
-        return <T>this.#plugins.get(name) || null;
-    }
-
-    /**
-     * Gets a list of all plugins that match `name`, with otr without a fragment.
-     *
-     * @param {string} basename - The base name of the plugin.
-     *
-     * @returns {NetworkPlugin[]} The list of plugins.
-     */
-    getPlugins<T extends NetworkPlugin = NetworkPlugin>(basename: string): Array<T> {
-        return <Array<T>>this.plugins.filter((p) => p.name.split('#')[0] === basename);
-    }
-
-    /**
      * Create a copy of this Network.
      */
     clone(): Network {
         const clone = new Network(this.name, this.chainId);
-        this.plugins.forEach((plugin) => {
-            clone.attachPlugin(plugin.clone());
-        });
         return clone;
     }
-
-    /**
-     * Compute the intrinsic gas required for a transaction.
-     *
-     * A GasCostPlugin can be attached to override the default values.
-     */
-    //    computeIntrinsicGas(tx: TransactionLike): number {
-    //        const costs = this.getPlugin<GasCostPlugin>("org.quais.plugins.network.GasCost") || (new GasCostPlugin());
-    //
-    //        let gas = costs.txBase;
-    //        if (tx.to == null) { gas += costs.txCreate; }
-    //        if (tx.data) {
-    //            for (let i = 2; i < tx.data.length; i += 2) {
-    //                if (tx.data.substring(i, i + 2) === "00") {
-    //                    gas += costs.txDataZero;
-    //                } else {
-    //                    gas += costs.txDataNonzero;
-    //                }
-    //            }
-    //        }
-    //
-    //        if (tx.accessList) {
-    //            const accessList = accessListify(tx.accessList);
-    //            for (const addr in accessList) {
-    //                gas += costs.txAccessListAddress + costs.txAccessListStorageKey * accessList[addr].storageKeys.length;
-    //            }
-    //        }
-    //
-    //        return gas;
-    //    }
 
     /**
      * Returns a new Network for the `network` name or chainId.
@@ -243,7 +141,6 @@ export class Network {
      * @returns {Network} The Network instance.
      */
     static from(network?: Networkish): Network {
-        injectCommonNetworks();
 
         // Default network
         if (network == null) {
@@ -313,148 +210,4 @@ export class Network {
         }
         Networks.set(nameOrChainId, networkFunc);
     }
-}
-
-type Options = {
-    altNames?: Array<string>;
-    plugins?: Array<NetworkPlugin>;
-};
-
-// We don't want to bring in formatUnits because it is backed by
-// FixedNumber and we want to keep Networks tiny. The values
-// included by the Gas Stations are also IEEE 754 with lots of
-// rounding issues and exceed the strict checks formatUnits has.
-function parseUnits(_value: number | string, decimals: number): bigint {
-    const value = String(_value);
-    if (!value.match(/^[0-9.]+$/)) {
-        throw new Error(`invalid gwei value: ${_value}`);
-    }
-
-    // Break into [ whole, fraction ]
-    const comps = value.split('.');
-    if (comps.length === 1) {
-        comps.push('');
-    }
-
-    // More than 1 decimal point or too many fractional positions
-    if (comps.length !== 2) {
-        throw new Error(`invalid gwei value: ${_value}`);
-    }
-
-    // Pad the fraction to 9 decimalplaces
-    while (comps[1].length < decimals) {
-        comps[1] += '0';
-    }
-
-    // Too many decimals and some non-zero ending, take the ceiling
-    if (comps[1].length > 9) {
-        let frac = BigInt(comps[1].substring(0, 9));
-        if (!comps[1].substring(9).match(/^0+$/)) {
-            frac++;
-        }
-        comps[1] = frac.toString();
-    }
-
-    return BigInt(comps[0] + comps[1]);
-}
-
-// Used by Polygon to use a gas station for fee data
-function getGasStationPlugin(url: string) {
-    return new FetchUrlFeeDataNetworkPlugin(url, async (fetchFeeData, provider, request) => {
-        // Prevent Cloudflare from blocking our request in node.js
-        request.setHeader('User-Agent', 'quais');
-
-        let response;
-        try {
-            const [_response, _feeData] = await Promise.all([request.send(), fetchFeeData()]);
-            response = _response;
-            const payload = response.bodyJson.standard;
-            const feeData = {
-                gasPrice: _feeData.gasPrice,
-                maxFeePerGas: parseUnits(payload.maxFee, 9),
-                maxPriorityFeePerGas: parseUnits(payload.maxPriorityFee, 9),
-            };
-            return feeData;
-        } catch (error: any) {
-            assert(
-                false,
-                `error encountered with polygon gas station (${JSON.stringify(request.url)})`,
-                'SERVER_ERROR',
-                { request, response, error },
-            );
-        }
-    });
-}
-
-// See: https://chainlist.org
-let injected = false;
-function injectCommonNetworks(): void {
-    if (injected) {
-        return;
-    }
-    injected = true;
-
-    /// Register popular Ethereum networks
-    function registerEth(name: string, chainId: number, options: Options): void {
-        const func = function () {
-            const network = new Network(name, chainId);
-
-            network.attachPlugin(new GasCostPlugin());
-
-            (options.plugins || []).forEach((plugin) => {
-                network.attachPlugin(plugin);
-            });
-
-            return network;
-        };
-
-        // Register the network by name and chain ID
-        Network.register(name, func);
-        Network.register(chainId, func);
-
-        if (options.altNames) {
-            options.altNames.forEach((name) => {
-                Network.register(name, func);
-            });
-        }
-    }
-
-    registerEth('mainnet', 1, { altNames: ['homestead'] });
-    registerEth('ropsten', 3, {});
-    registerEth('rinkeby', 4, {});
-    registerEth('goerli', 5, {});
-    registerEth('kovan', 42, {});
-    registerEth('sepolia', 11155111, {});
-
-    registerEth('classic', 61, {});
-    registerEth('classicKotti', 6, {});
-
-    registerEth('arbitrum', 42161, {});
-    registerEth('arbitrum-goerli', 421613, {});
-
-    registerEth('base', 8453, {});
-    registerEth('base-goerli', 84531, {});
-    registerEth('base-sepolia', 84532, {});
-
-    registerEth('bnb', 56, {});
-    registerEth('bnbt', 97, {});
-
-    registerEth('linea', 59144, {});
-    registerEth('linea-goerli', 59140, {});
-
-    registerEth('matic', 137, {
-        plugins: [getGasStationPlugin('https://gasstation.polygon.technology/v2')],
-    });
-    registerEth('matic-mumbai', 80001, {
-        altNames: ['maticMumbai', 'maticmum'], // @TODO: Future remove these alts
-        plugins: [getGasStationPlugin('https://gasstation-testnet.polygon.technology/v2')],
-    });
-
-    registerEth('optimism', 10, {
-        
-        plugins: [],
-    });
-    registerEth('optimism-goerli', 420, {});
-
-    registerEth('xdai', 100, {});
 }
