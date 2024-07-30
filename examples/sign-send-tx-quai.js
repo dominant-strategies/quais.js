e2aea011bc38e72eee1c80509d3920400a31b1e8
Alejo Acosta <alejoacos@gmail.com>
2024-07-30 17:28:20 -0400

    adds 'examples' folder

---
+++ examples/sign-send-tx-quai.js
@@ -0,0 +1,42 @@
+const quais = require('../lib/commonjs/quais');
+require('dotenv').config();
+
+async function main() {
+	
+	// create provider
+	const provider = new quais.JsonRpcProvider(process.env.RPC_URL);
+	
+	// create wallet
+	const mnemonic = quais.Mnemonic.fromPhrase(process.env.MNEMONIC);
+	const quaiWallet = quais.QuaiHDWallet.fromMnemonic(mnemonic);
+	quaiWallet.connect(provider);
+
+	// Create tx
+	const addressInfo1 = await quaiWallet.getNextAddress(0, quais.Zone.Cyprus1);
+	const from = addressInfo1.address;
+	const txObj = new quais.QuaiTransaction(from);
+	txObj.to = "0x002F4783248e2D6FF1aa6482A8C0D7a76de3C329";
+	txObj.value = BigInt(4200000);
+	/*
+	 * The following fields are optional, but can be set as follows:
+	 * txObj.chainId = BigInt(9000);
+	 * txObj.nonce = await provider.getTransactionCount(from, 'latest');
+	 * txObj.gasLimit = BigInt(1000000);
+	 * txObj.maxPriorityFeePerGas = BigInt(10000000000),
+	 * txObj.maxFeePerGas = BigInt(30000000000000),
+	 */
+
+	// Sign and send the transaction
+	const tx = await quaiWallet.sendTransaction(txObj);
+
+	// Wait for tx to be mined
+	const txReceipt = await tx.wait();
+	console.log('\nTx receipt:', txReceipt);
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error)
+		process.exit(1)
+	})
