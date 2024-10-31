dfc901b86b51c646cf6d1c66d2f578b2af7540f0
robertlincecum <rglincecum@gmail.com>
2024-10-31 16:57:57 -0500

    remove extra logs

--- src/providers/provider-jsonrpc.ts
+++ src/providers/provider-jsonrpc.ts
@@ -998,7 +998,7 @@ export abstract class JsonRpcApiProvider<C = FetchRequest> extends AbstractProvi
 
         (async () => {
             let retries = 0;
-            const maxRetries = 2;
+            const maxRetries = 5;
             while (this.#network == null && !this.destroyed && retries < maxRetries) {
                 try {
                     this.#network = await this._detectNetwork();
@@ -1629,16 +1629,11 @@ export class JsonRpcProvider extends JsonRpcApiProvider {
     }
 
     async send(method: string, params: Array<any> | Record<string, any>, shard?: Shard, now?: boolean): Promise<any> {
-        console.log('send', method, params, shard, now);
         try {
             this._start();
 
-            console.log('started');
-
             return await super.send(method, params, shard, now);
         } catch (error) {
-            console.log('hiiiiiiii');
-            console.log('caught error', error);
             return Promise.reject(error);
         }
         // All requests are over HTTP, so we can just start handling requests
@@ -1693,7 +1688,6 @@ export class JsonRpcProvider extends JsonRpcApiProvider {
 
             return resp;
         } catch (error) {
-            console.log('caught error');
             return [
                 {
                     id: Array.isArray(payload) ? payload[0].id : payload.id,
