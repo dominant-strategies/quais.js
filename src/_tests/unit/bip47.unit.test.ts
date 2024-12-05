d089d0a002138d590685cd33e749b7093d18b586
Alejo Acosta <alejoacos@gmail.com>
2024-12-05 13:38:44 -0300

    fix bip47 payment code unit tests

--- src/_tests/unit/bip47.unit.test.ts
+++ src/_tests/unit/bip47.unit.test.ts
@@ -1,44 +1,42 @@
 import { Mnemonic, QiHDWallet, Zone } from '../../index.js';
 import assert from 'assert';
 
+const ALICE_MNEMONIC =
+    'empower cook violin million wool twelve involve nice donate author mammal salt royal shiver birth olympic embody hello beef suit isolate mixed text spot';
+const BOB_MNEMONIC = 'innocent perfect bus miss prevent night oval position aspect nut angle usage expose grace juice';
+
 describe('Test generation of payment codes and payment addresses', function () {
     this.timeout(10000);
-    const ALICE_MNEMONIC =
-        'empower cook violin million wool twelve involve nice donate author mammal salt royal shiver birth olympic embody hello beef suit isolate mixed text spot';
     const aliceMnemonic = Mnemonic.fromPhrase(ALICE_MNEMONIC);
     const aliceQiWallet = QiHDWallet.fromMnemonic(aliceMnemonic);
 
-    const BOB_MNEMONIC =
-        'innocent perfect bus miss prevent night oval position aspect nut angle usage expose grace juice';
     const bobMnemonic = Mnemonic.fromPhrase(BOB_MNEMONIC);
     const bobQiWallet = QiHDWallet.fromMnemonic(bobMnemonic);
 
-    it('generates payment codes and payment addresses for Bob and Alice', async function () {
-        const alicePaymentCode = await aliceQiWallet.getPaymentCode(0);
+    it('generates payment codes and payment addresses for Bob and Alice', function () {
+        const alicePaymentCode = aliceQiWallet.getPaymentCode(0);
         assert.equal(
             alicePaymentCode,
-            'PM8TJTzqM3pqdQxBA52AX9M5JBCdkyJYWpNfJZpNX9H7FY2XitYFd99LSfCCQamCN5LubK1YNQMoz33g1WgVNX2keWoDtfDG9H1AfGcupRzHsPn6Rc2z',
+            'PM8TJMwuDmEtpL9JNqRpre1RvimtqQvwzGHZr8S95HjKmabApLkSPyprsaUZAzWxAscgBbJo2XRqrkD649YZB9qU9HkNFVGoaN9UYv2DCGrcErz21Nfz',
         );
 
-        const bobPaymentCode = await bobQiWallet.getPaymentCode(0);
+        const bobPaymentCode = bobQiWallet.getPaymentCode(0);
         assert.equal(
             bobPaymentCode,
-            'PM8TJaDZL8og3dTyeBF2DFZnhiNAKr5evrNRVJhwi3bMt6ZLvTu3wVQApup7bf5R4bYc1mxvzQzFsrTabv8B3E2syDgzHwGcQUzLWrf5Nt2A2K6kdeAC',
+            'PM8TJJYDFEugmzgwU9EoT3xEhiEy5tPLJCxcwa9HFEM2bs2zsGRdxpkJwsKXi2u3Tuu5AK3bUoethFD3oDB2r2vnUJv4W9sGWdvffNUriHS1D1szfbxn',
         );
 
         // Alice generates a payment address for sending funds to Bob
-        const bobAddress = await aliceQiWallet.getNextSendAddress(bobPaymentCode, Zone.Cyprus1);
-        assert.equal(bobAddress, '0x0083d552Fc0A3f9269089cbb9Ca11eaba93802e3');
+        const bobInfoAddress = aliceQiWallet.getNextSendAddress(bobPaymentCode, Zone.Cyprus1);
+        assert.equal(bobInfoAddress.address, '0x00aFce8641EE61598B582ea02Df96623280E55d9');
 
         // Bob generates a payment address for receiving funds from Alice
-        const receiveAddress = await bobQiWallet.getNextReceiveAddress(alicePaymentCode, Zone.Cyprus1);
-        assert.equal(receiveAddress, '0x0083d552Fc0A3f9269089cbb9Ca11eaba93802e3');
+        const receiveInfoAddress = bobQiWallet.getNextReceiveAddress(alicePaymentCode, Zone.Cyprus1);
+        assert.equal(receiveInfoAddress.address, '0x00aFce8641EE61598B582ea02Df96623280E55d9');
     });
 });
 
 describe('Test opening channels', function () {
-    const BOB_MNEMONIC =
-        'innocent perfect bus miss prevent night oval position aspect nut angle usage expose grace juice';
     const bobMnemonic = Mnemonic.fromPhrase(BOB_MNEMONIC);
     const bobQiWallet = QiHDWallet.fromMnemonic(bobMnemonic);
     it('opens a channel correctly', async function () {
