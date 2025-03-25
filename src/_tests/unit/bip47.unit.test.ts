653c1d0f2877f17272a5e07e375336f9aec7fbb8
Alejo Acosta <alejoacos@gmail.com>
2025-03-25 11:28:56 -0300

    update bip47.unit.test data

--- src/_tests/unit/bip47.unit.test.ts
+++ src/_tests/unit/bip47.unit.test.ts
@@ -1,9 +1,11 @@
 import { Mnemonic, QiHDWallet, Zone } from '../../index.js';
 import assert from 'assert';
 
-const ALICE_MNEMONIC =
-    'empower cook violin million wool twelve involve nice donate author mammal salt royal shiver birth olympic embody hello beef suit isolate mixed text spot';
-const BOB_MNEMONIC = 'innocent perfect bus miss prevent night oval position aspect nut angle usage expose grace juice';
+// Data taken from the test vectors in
+// https://gist.github.com/SamouraiDev/6aad669604c5930864bd
+
+const ALICE_MNEMONIC = 'response seminar brave tip suit recall often sound stick owner lottery motion';
+const BOB_MNEMONIC = 'reward upper indicate eight swift arch injury crystal super wrestle already dentist';
 
 describe('Test generation of payment codes and payment addresses', function () {
     this.timeout(10000);
@@ -13,28 +15,47 @@ describe('Test generation of payment codes and payment addresses', function () {
     const bobMnemonic = Mnemonic.fromPhrase(BOB_MNEMONIC);
     const bobQiWallet = QiHDWallet.fromMnemonic(bobMnemonic);
 
-    it('generates payment codes and payment addresses for Bob and Alice', function () {
-        const alicePaymentCode = aliceQiWallet.getPaymentCode(0);
+    let alicePaymentCode: string;
+    let bobPaymentCode: string;
+
+    it('generates payment codes correctly', function () {
+        alicePaymentCode = aliceQiWallet.getPaymentCode(0);
         assert.equal(
             alicePaymentCode,
-            'PM8TJMwuDmEtpL9JNqRpre1RvimtqQvwzGHZr8S95HjKmabApLkSPyprsaUZAzWxAscgBbJo2XRqrkD649YZB9qU9HkNFVGoaN9UYv2DCGrcErz21Nfz',
+            'PM8TJNrUrAMCZen7rKygy8kk6q2wR4a8iXXk23WNZKoSqoZGFpcYBKtmknSgYREK6q95S6Zn61YVnUEutCfMc2oVyxNJw95bdXHPdUDAw2h3QETitdY5',
         );
 
-        const bobPaymentCode = bobQiWallet.getPaymentCode(0);
+        bobPaymentCode = bobQiWallet.getPaymentCode(0);
         assert.equal(
             bobPaymentCode,
-            'PM8TJJYDFEugmzgwU9EoT3xEhiEy5tPLJCxcwa9HFEM2bs2zsGRdxpkJwsKXi2u3Tuu5AK3bUoethFD3oDB2r2vnUJv4W9sGWdvffNUriHS1D1szfbxn',
+            'PM8TJa1iesVFYQTARdp4H278QXooMMXkhsMXakjicvpLJnHSTbUdXhrKsAJmeV89LzZx5uTKGcVM98FwPMEcza4sfDXtQDiLY3HfoJLegdXiEZ7F8B2s',
         );
-
+    });
+    it('generates addresses when Alice sends funds to Bob', function () {
         // Alice generates a payment address for sending funds to Bob
         aliceQiWallet.openChannel(bobPaymentCode);
-        const bobInfoAddress = aliceQiWallet.getNextSendAddress(bobPaymentCode, Zone.Cyprus1);
-        assert.equal(bobInfoAddress.address, '0x00aFce8641EE61598B582ea02Df96623280E55d9');
+        const bobAddressInfo = aliceQiWallet.getNextSendAddress(bobPaymentCode, Zone.Cyprus1);
+        assert.equal(bobAddressInfo.index, 68);
+        assert.equal(bobAddressInfo.address, '0x00bF928076eF7a4230B94d93aece96aEd0898E96');
 
         // Bob generates a payment address for receiving funds from Alice
         bobQiWallet.openChannel(alicePaymentCode);
-        const receiveInfoAddress = bobQiWallet.getNextReceiveAddress(alicePaymentCode, Zone.Cyprus1);
-        assert.equal(receiveInfoAddress.address, '0x00aFce8641EE61598B582ea02Df96623280E55d9');
+        const aliceAddressInfo = bobQiWallet.getNextReceiveAddress(alicePaymentCode, Zone.Cyprus1);
+        assert.equal(aliceAddressInfo.index, 68);
+        assert.equal(aliceAddressInfo.address, '0x00bF928076eF7a4230B94d93aece96aEd0898E96');
+    });
+    it('generates addresses when Bob sends funds to Alice', function () {
+        // Bob generates a payment address for sending funds to Alice
+        bobQiWallet.openChannel(alicePaymentCode);
+        const aliceInfoAddress = bobQiWallet.getNextSendAddress(alicePaymentCode, Zone.Cyprus1);
+        assert.equal(aliceInfoAddress.index, 129);
+        assert.equal(aliceInfoAddress.address, '0x0086458162879D446F94eB3156cAAA4E9d49d417');
+
+        // Alice generates a payment address for receiving funds from Bob
+        aliceQiWallet.openChannel(bobPaymentCode);
+        const bobAddressInfo = aliceQiWallet.getNextReceiveAddress(bobPaymentCode, Zone.Cyprus1);
+        assert.equal(bobAddressInfo.index, 129);
+        assert.equal(bobAddressInfo.address, '0x0086458162879D446F94eB3156cAAA4E9d49d417');
     });
 });
 
