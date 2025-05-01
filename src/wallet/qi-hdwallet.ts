afbbcbc147b27661e643b0a9e49941972d563611
Jonathan Downing <jdowning@utexas.edu>
2025-05-01 11:49:59 -0500

    Bugfix: getOutpoints should also return payment channel outpoints

--- src/wallet/qi-hdwallet.ts
+++ src/wallet/qi-hdwallet.ts
@@ -389,7 +389,10 @@ export class QiHDWallet extends AbstractHDWallet<QiAddressInfo> {
         const bip44ExternalOutpoints = this.externalBip44.getOutpoints(zone);
         const bip44ChangeOutpoints = this.changeBip44.getOutpoints(zone);
         const privatekeyOutpoints = this.privatekeyWallet.getOutpoints(zone);
-        return [...bip44ExternalOutpoints, ...bip44ChangeOutpoints, ...privatekeyOutpoints];
+        const paymentChannelOutpoints = Array.from(this.paymentChannels.values()).flatMap((pc) =>
+            pc.selfWallet.getOutpoints(zone),
+        );
+        return [...bip44ExternalOutpoints, ...bip44ChangeOutpoints, ...privatekeyOutpoints, ...paymentChannelOutpoints];
     }
 
     /**
